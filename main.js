b6a5d1df4c4fd3466d5c20a54e4f2be4ac8edd29
saim514 <81713694+saim514@users.noreply.github.com>
2022-03-14 17:34:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload()
+{}
+
+function draw()
+{}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+    background("white");
+}
+
+function clearCanvas()
+{
+    background("white")
+    console.log("Canvas is cleared")
+}
