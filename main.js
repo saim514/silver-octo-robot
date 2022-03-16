b10208e257408b069267abae1c4d343b8034ef8b
saim514 <81713694+saim514@users.noreply.github.com>
2022-03-16 18:53:26 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,47 @@
-function preload()
-{}
-
-function draw()
-{}
-
 function setup()
 {
     canvas = createCanvas(300,300);
     canvas.center();
     background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function preload()
+{
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function draw()
+{
+    strokeWeight(7);
+    stroke("grey");
+    if(mouseIsPressed)
+    {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
 }
 
 function clearCanvas()
 {
     background("white")
     console.log("Canvas is cleared")
+}
+
+function classifyCanvas()
+{
+ classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+     console.error(error);
+    }
+    console.log(results);
+    document.getElementById('item').innerHTML = 'Item: ' + results[0].label;
+    document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+    var utterthis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterthis);
 }
